440ccefc464e58e7c715216b362f5cc00a76f4cb
unknown <raulalfonzo66@gmail.com>
2021-12-19 05:45:15 -0400

    Create and mount Vue App

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
-const product = 'Socks'
+/* First, lets create our Vue App */
+const app = Vue.createApp({
+    data: function(){
+        return{
+            product: 'Socks'
+        }
+    }
+}).mount('#app')
